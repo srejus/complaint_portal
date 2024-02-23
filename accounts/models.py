ae5311cc34b98798dc72a0e0613319a8281a2500
srejus <srejusjayakumar@gmail.com>
2024-02-23 09:47:29 +0530

    admin changes

--- accounts/models.py
+++ accounts/models.py
@@ -13,7 +13,7 @@ class Account(models.Model):
     full_name = models.CharField(max_length=100,null=True,blank=True)
     email = models.EmailField(null=True,blank=True)
     phone = models.CharField(max_length=15,null=True,blank=True)
-    user_type = models.CharField(max_length=100,default='USER')
+    user_type = models.CharField(max_length=100,default='USER',choices=USER_TYPE_CHOICES)
     address = models.CharField(max_length=200,null=True,blank=True)
     state = models.CharField(max_length=50,null=True,blank=True)
     pincode = models.CharField(max_length=6,null=True,blank=True)
